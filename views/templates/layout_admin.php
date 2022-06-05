7369d22bd6a4106765b1043af0cba73334926b66
maibelline <mayb80@hotmail.fr>
2022-06-05 16:44:49 +0200

    improved admin style

--- views/templates/layout_admin.php
+++ views/templates/layout_admin.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/assets/css/homePageStyle.css">
     <link rel="stylesheet" href="/../../public/assets/css/<?= $headerDashStyle ?>">
     <link rel="stylesheet" href="/../../public/assets/css/<?= $choiceQuizStyle ?>">
     <link rel="stylesheet" href="/../../public/assets/css/<?= $profilUserStyle ?>">
@@ -16,9 +17,10 @@
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-3">
+    <?php require_once dirname(__FILE__) . '/header_bar.php';?>
+    <div class="container-fluid ms-0">
+        <div class="row ms-0">
+            <div class="col-3 ms-0">
                 <?php require_once dirname(__FILE__) . '/sidebar_bar_admin.php'; ?>
             </div>
             <div class="col-9">
