7f9f42e0b99106da86647f94ab7302a9d82f3d60
maibelline <mayb80@hotmail.fr>
2022-06-09 09:44:14 +0200

    commit

--- views/memory.php
+++ views/memory.php
@@ -1,7 +1,7 @@
 
-<div class="container mt-3">
+<div class="container-fluid mt-3 pb-5">
     <div class="row">
-        <iframe class="col-4 offset-4" src="https://maibelline.github.io/Memo/" height="650px" style="background:rgba(0,0,0,.5)"></iframe>
+        <iframe class="col-12 pb-5" src="https://maibelline.github.io/Memo/" height="750px" style="background:rgba(0,0,0,.5)"></iframe>
     </div>
 </div>
 
